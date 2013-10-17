bad6265417eb7cc674c6e723dd2002a98ef672eb
Bernardo <bernardorio@gmail.com>
2013-10-17 19:59:37 +0100

    Adding the barebones for plugin initialisation and integration with hammer

--- src/js/core.js
+++ src/js/core.js
@@ -1,7 +1,22 @@
-var FondlePic = function(options) {
-	this._options = {
-			someOption: true
+var FondlePic = function(element, options) {
+	console.log(element);
+    return new FondlePic.Instance(element, options || {});
+};
+
+FondlePic.Instance = function(element, options) {
+	var self = this;
+	this.canvasElement = element;
+	
+	initialise = function(){
+		self.touchElement = Hammer(self.canvasElement, {
+	        drag: false,
+	        transform: false
+	    });
+	
+		self.touchElement.on("tap", function(event) {
+		     alert("can't touch this");
+		});
 	};
 	
-	
+	initialise();
 };
