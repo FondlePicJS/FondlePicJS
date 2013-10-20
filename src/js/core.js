2e8d48fc911a2074b73aa7b60855dcaf6b5ed777
Bernardo <bernardorio@gmail.com>
2013-10-20 15:05:26 +0100

    Allowing image to be displayed into canvas

--- src/js/core.js
+++ src/js/core.js
@@ -3,9 +3,16 @@ var FondlePic = function(element, options) {
     return new FondlePic.Instance(element, options || {});
 };
 
-FondlePic.Instance = function(element, options) {
+FondlePic.Instance = function(elementId, options) {
 	var self = this;
-	this.canvasElement = element;
+
+	this.imageFile = document.getElementById(options['imageFile']);
+	this.canvasElement = document.getElementById(elementId);
+	this.canvasContext = this.canvasElement.getContext('2d');
+	this.canvasWidth = this.canvasElement.width;
+	this.canvasHeight = this.canvasElement.height;	
+	this.imageObj = new Image();
+	this.base64ImageData = '';
 	
 	initialise = function(){
 		self.touchElement = Hammer(self.canvasElement, {
@@ -16,6 +23,26 @@ FondlePic.Instance = function(element, options) {
 		self.touchElement.on("tap", function(event) {
 		     alert("can't touch this");
 		});
+	};
+	
+	this.imageFile.onchange = function(event) {
+		var reader = new FileReader(),
+			file = self.imageFile.files[0];
+	
+		reader.onload = function(event) {
+			self.base64ImageData = event.target.result;
+			self.imageObj.src = self.base64ImageData;
+		}
+		reader.readAsDataURL(file);
+	};
+	
+	this.imageObj.onload = function() {
+		paintCanvas();
+	}
+	
+	paintCanvas = function(){
+		console.log(self.imageObj.src);
+		self.canvasContext.drawImage(self.imageObj, 0 , 0, 300, 500);
 	};
 	
 	initialise();
